0b3d8cf46b3d59468fba06a561193ab117275847
Eli Cusic <tntitans31@gmail.com>
2020-06-10 20:57:58 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function frameIt(){
 
 function pressIt(){
   $('#typing').on('keydown', function(){
-    if (event.which === 71){
+    if (key.which == 71){
       window.alert('G was pressed')
     }
   })
